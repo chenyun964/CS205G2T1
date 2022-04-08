e5fb84a0e0cbd0a31f45c98e1869a8b78f871c72
Chen Yun <chenyun964@gmail.com>
2022-04-09 01:39:38 +0800

    Fix UI

--- app/src/main/java/com/example/serviceexample/MainActivity.java
+++ app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.serviceexample;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.util.Log;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -46,10 +53,17 @@ public class MainActivity extends AppCompatActivity {
         String uri = "@drawable/rounded_ticket";  // where myresource (without the extension) is the file
         int imageResource = getResources().getIdentifier(uri, null, getPackageName());
 
+
         // register broadcast receiver to get informed that data is downloaded so that we can calc
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int childCount = listView.getChildCount();
+                if (childCount >= 5) {
+                    Log.v("Input","Max ticket added");
+                    return;
+                }
+
                 String tickerText = ticker.getText().toString();
                 Log.v("Ticker Name:", tickerText);
                 if (tickerText.matches("")) {
@@ -57,43 +71,47 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-//                LinearLayout container = new LinearLayout(MainActivity.this);
-//                container.setOrientation(LinearLayout.HORIZONTAL);
-//
-//                LayoutParams params = (LayoutParams) container.getLayoutParams();
-//                params.width = 370;
-//                params.height = 50;
-//                view.setLayoutParams(params);
-//                Drawable bg = getResources().getDrawable(imageResource);
-//                container.setBackground(bg);
+                LinearLayout container = new LinearLayout(MainActivity.this);
+                container.setOrientation(LinearLayout.HORIZONTAL);
+                LayoutParams params = new LayoutParams(dpTopx(370, view), LayoutParams.WRAP_CONTENT);
+                params.setMargins(dpTopx(24, view), dpTopx(10, view), 0, 0);
+                container.setLayoutParams(params);
+                container.setPadding(dpTopx(15, view), dpTopx(15, view), dpTopx(15, view), dpTopx(15, view));
+
+                Drawable bg = getResources().getDrawable(imageResource);
+                container.setBackground(bg);
 
                 TextView nTicker = new TextView(MainActivity.this);
-                Log.v("txt", tickerText);
-                nTicker.setText(tickerText);
-                nTicker.setHeight(50);
-                nTicker.setWidth(50);
-//
-//                TextView nReturn = new TextView(MainActivity.this);
-//                Log.v("txt", tickerText);
-//                nReturn.setText(tickerText);
-//                nReturn.setHeight(50);
-//                nReturn.setWidth(50);
-//
-//                TextView nVolat = new TextView(MainActivity.this);
-//                Log.v("txt", tickerText);
-//                nVolat.setText(tickerText);
-//                nVolat.setHeight(50);
-//                nVolat.setWidth(50);
-//
-//                container.addView(nTicker);
-//                container.addView(nReturn);
-//                container.addView(nVolat);
+                nTicker.setText(tickerText.toUpperCase(Locale.ROOT));
+                nTicker.setTextColor(Color.parseColor("#FFFFFF"));
+                nTicker.setLayoutParams(new TableLayout.LayoutParams(dpTopx(60, view), dpTopx(24, view), 1f));
 
-                listView.addView(nTicker);
+                TextView nReturn = new TextView(MainActivity.this);
+                nReturn.setText("Calculating");
+                nReturn.setGravity(Gravity.CENTER);
+                nReturn.setTextColor(Color.parseColor("#FFFF4444"));
+                nReturn.setLayoutParams(new TableLayout.LayoutParams(dpTopx(80, view), dpTopx(24, view), 1f));
 
-                int childCount = listView.getChildCount();
-                Log.v("child count:", Integer.toString(childCount));
+
+                TextView nVolat = new TextView(MainActivity.this);
+                nVolat.setText("Calculating");
+                nVolat.setGravity(Gravity.CENTER);
+                nVolat.setTextColor(Color.parseColor("#FF99CC00"));
+                nVolat.setLayoutParams(new TableLayout.LayoutParams(dpTopx(80, view), dpTopx(24, view), 1f));
+
+                container.addView(nTicker);
+                container.addView(nReturn);
+                container.addView(nVolat);
+
+                listView.addView(container);
+                childCount += 1;
                 countLabel.setText(childCount + " / 5 Ticket Added");
+                ticker.setText("");
+                if(childCount >= 5){
+                    ticker.clearFocus();
+                    hideSoftKeyboard(MainActivity.this, view);
+                }
+
 //                Intent intent = new Intent(getApplicationContext(), MyService.class);
 //                intent.putExtra("ticker", String.valueOf(ticker.getText()));
 //                startService(intent);
@@ -103,6 +121,17 @@ public class MainActivity extends AppCompatActivity {
 //                registerReceiver(myBroadcastReceiver, new IntentFilter("DOWNLOAD_COMPLETE"));
             }
         });
+    }
+
+    public static void hideSoftKeyboard (Activity activity, View view)
+    {
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
+    }
+
+    private int dpTopx(int dp, View view){
+        float scale = view.getContext().getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 
     @Override
